94c36a381521691e65d830595290e300ce054b85
Katie Macke <katie@sleepysheepy.dev>
2023-10-12 01:06:35 -0400

    rename

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,7 @@ import './globals.css'
 import type { Metadata } from 'next'
 
 export const metadata: Metadata = {
-  title: 'What is Goalie Interference?',
+  title: 'HockeyVote',
 }
 
 export default function RootLayout({
