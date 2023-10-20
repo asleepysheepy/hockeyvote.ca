95017050655c94b987939e9978963e21a9817c93
Katie Macke <katie@sleepysheepy.dev>
2023-10-20 02:04:36 -0400

    databases and builds pat 2

--- prisma/migrations/20231020054106_init/migration.sql
+++ prisma/migrations/20231020054106_init/migration.sql
@@ -1,8 +1,8 @@
 -- CreateTable
 CREATE TABLE "Incident" (
-    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
-    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    "updatedAt" DATETIME NOT NULL,
+    "id" SERIAL NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
     "youtubeId" TEXT NOT NULL,
     "startTime" INTEGER NOT NULL,
     "endTime" INTEGER NOT NULL,
@@ -10,24 +10,30 @@ CREATE TABLE "Incident" (
     "offendingPlayerTeam" TEXT NOT NULL,
     "goalie" TEXT NOT NULL,
     "goalieTeam" TEXT NOT NULL,
-    "date" DATETIME NOT NULL,
+    "date" TIMESTAMP(3) NOT NULL,
     "period" INTEGER NOT NULL,
     "timeElapsed" TEXT NOT NULL,
     "callOnIce" TEXT NOT NULL,
-    "endResult" TEXT NOT NULL
+    "endResult" TEXT NOT NULL,
+
+    CONSTRAINT "Incident_pkey" PRIMARY KEY ("id")
 );
 
 -- CreateTable
 CREATE TABLE "Vote" (
-    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
-    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    "updatedAt" DATETIME NOT NULL,
+    "id" SERIAL NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
     "incidentId" INTEGER NOT NULL,
     "wasGoalieInterference" BOOLEAN NOT NULL,
     "fanOfEitherTeam" BOOLEAN NOT NULL,
     "fanOfWhichTeam" TEXT,
-    CONSTRAINT "Vote_incidentId_fkey" FOREIGN KEY ("incidentId") REFERENCES "Incident" ("id") ON DELETE CASCADE ON UPDATE CASCADE
+
+    CONSTRAINT "Vote_pkey" PRIMARY KEY ("id")
 );
 
 -- CreateIndex
 CREATE INDEX "Incident_youtubeId_idx" ON "Incident"("youtubeId");
+
+-- AddForeignKey
+ALTER TABLE "Vote" ADD CONSTRAINT "Vote_incidentId_fkey" FOREIGN KEY ("incidentId") REFERENCES "Incident"("id") ON DELETE CASCADE ON UPDATE CASCADE;
--- test/db-utils.ts
+++ test/db-utils.ts
@@ -1,15 +1,11 @@
 import { type PrismaClient } from '@prisma/client'
 
 export async function cleanupDb(prisma: PrismaClient) {
-  const tables = await prisma.$queryRaw<
-    { name: string }[]
-  >`SELECT name FROM sqlite_master WHERE type='table' AND name NOT LIKE 'sqlite_%' AND name NOT LIKE '_prisma_migrations';`
+  const votes = await prisma.vote.findMany({})
+  await Promise.all(votes.map((vote) => prisma.vote.delete({ where: { id: vote.id } })))
 
-  await prisma.$transaction([
-    // Disable FK constraints to avoid relation conflicts during deletion
-    prisma.$executeRawUnsafe(`PRAGMA foreign_keys = OFF`),
-    // Delete all rows from each table, preserving table structures
-    ...tables.map(({ name }) => prisma.$executeRawUnsafe(`DELETE from "${name}"`)),
-    prisma.$executeRawUnsafe(`PRAGMA foreign_keys = ON`),
-  ])
+  const incidents = await prisma.incident.findMany({})
+  await Promise.all(
+    incidents.map((incident) => prisma.incident.delete({ where: { id: incident.id } })),
+  )
 }
